1544595fe160a829148a10e8c1d5e83cc69eabca
SethSorrellDev <sethm545@gmail.com>
2026-07-25 17:15:51 -0400

    Phase 0: RouteBook scaffold - Spring Boot 3.5.16, Java 21, Maven wrapper, H2/actuator config

---
+++ src/main/java/com/seth/routebook/RouteBookApplication.java
@@ -0,0 +1,13 @@
+package com.seth.routebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seth/routebook/RouteBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seth.routebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
